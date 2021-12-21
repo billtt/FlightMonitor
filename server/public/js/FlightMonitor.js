c8ae3afdeb6fcdd7b802940fe986914244165d96
Bill Z <bill@magictavern.com>
2021-12-21 21:14:16 +0800

    Rewrite status page with Bootstrap

--- server/public/js/FlightMonitor.js
+++ server/public/js/FlightMonitor.js
@@ -3,6 +3,8 @@
  */
 
 let _data = null;
+const NM2KM = 1.852;
+const FT2M = 0.3048;
 
 function getStatus() {
     $.getJSON('/status', (data) => {
@@ -18,7 +20,7 @@ function getStatus() {
 
 function update(prop, value) {
     if ($.isNumeric(value)) {
-        value = value.toFixed(1);
+        value = value.toLocaleString('en-US', {maximumFractionDigits: 1});
     }
     $('#' + prop).text(value);
 }
@@ -51,14 +53,31 @@ function updateAll() {
     let seconds = ((Date.now() - timestamp.getTime()) / 1000).toFixed(0);
     updateTimeColor(seconds);
     update('valUpdateTime', `${_data.timestamp} (${seconds}s)`);
+
     update('valGS', _data.GS);
+    update('valGSKm', _data.GS * NM2KM);
     update('valTAS', _data.TAS);
+
     update('valFuel', _data.fuelWeight * 0.453592);
     update('valFuelRate', _data.fuelPerHour * 0.453592);
     update('valFuelTime', _data.fuelWeight / _data.fuelPerHour);
-    update('valDistance', _data.distance);
+
+    let remainingDist = _data.distance;
+    // converting from M to NM
+    let totalDist = _data.totalDistance / 1000 / NM2KM;
+    let completedDist = Math.max(0, totalDist - remainingDist);
+    update('valDistance', remainingDist);
+    update('valDistanceKm', remainingDist * NM2KM);
+    update('valTotalDist', totalDist);
+    update('valTotalDistKm', totalDist * NM2KM);
+    update('valCompletedDist', completedDist);
+    update('valCompletedDistKm', completedDist * NM2KM);
+
+    update('valAltitude', _data.altitude);
+    update('valAltitudeM', _data.altitude * FT2M);
+
     let percent = 100 - (_data.distance / _data.totalDistance * 1852 * 100);
-    update('valDistPercent', percent);
+    $('#pgbPercent').css('width', percent + '%');
     let ete = _data.ETE;
     update('valETE', getDisplayTimeSpan(ete));
     update('valETA', moment().add(ete - seconds, 's').format('MM/DD HH:mm'));
